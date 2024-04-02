2fee27b8d9fd036f1eb13d2e1700ddbdac3288cf
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-04-02 21:02:49 +0530

    Update 9.cpp

--- Step 1/Lec 2 (Patterns)/9.cpp
+++ Step 1/Lec 2 (Patterns)/9.cpp
@@ -9,11 +9,11 @@ void nStarDiamond(int n) {
         cout << endl;
     }
     for (int i=n; i>0; i--) {
+        for (int j=0; j<n-i; j++) {
+            cout << " ";
+        }
         for (int j=0; j<2*i-1; j++) {
             cout << "*";
-        }
-        for (int j=0; j<n-i+1; j++) {
-            cout << " ";
         }
         cout << endl;
     }
